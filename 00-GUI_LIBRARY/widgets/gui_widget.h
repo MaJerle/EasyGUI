1645e23e7da43fa09e35bb7ecebfc7209bb905de
Tilen Majerle <tilen.majerle@gmail.com>
2017-06-06 01:13:46 +0200

    Brief description

--- 00-GUI_LIBRARY/widgets/gui_widget.h
+++ 00-GUI_LIBRARY/widgets/gui_widget.h
@@ -27,6 +27,7 @@
     OTHER DEALINGS IN THE SOFTWARE.
    ----------------------------------------------------------------------
 \endverbatim
+ *
  */
 #ifndef GUI_WIDGET_H
 #define GUI_WIDGET_H
@@ -43,8 +44,41 @@ extern "C" {
  *
  * \section sect_widgets Widget organization
  *
- * Widgets are organized in multi depth linked list of parent widgets, illustrated below:
+ * Widgets are organized in multi depth linked list of parent widgets where each widget has reference to:
+ *  - Next widget
+ *  - Previous widget
+ *  - Parent widget
+ *  - Children first widget
+ *  - Children last widget
  *
+ * \par Next widget link
+ *
+ * Next widget in linked list is used to get next widget for drawing operation
+ * Widget which is at the end of linked list is later visible on top on LCD.
+ *
+ * Drawing operations starts with 
+ *
+ * \par Previous widget link
+ *
+ * Previous link is used to get widget which is "behind" current widget. 
+ * This link is used for touch management where widget at the end of linked list is first checked for events (touch, mouse, etc)
+ *
+ * \par Parent widget
+ * 
+ * Link to parent widget is always required to calculate currect widget position on LCD and for other tasks (like percentage width relative to parent, etc)
+ *
+ * \par Children first widget
+ *
+ * Children links are supported on widgets which implement \ref GUI_WIDGET_ROOT_t structure to handle children widgets (such as windows).
+ * It hold link to less important widget (drawed first, less visible in some cases)
+ *
+ * \par Children last widget
+ *
+ * Children last widget is used for events processing (touch, mouse, etc)
+ *
+ *
+ * Any operation on widgets structure (change Z-index, put on front, etc) is only moving widgets on linked list.
+ * Linked list is most important part of GUI library therefore it is designed in robust way.
  */
     
 /**
